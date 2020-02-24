16d31bd7a6145242caab6c051a43d601e6dcff8c
AlejandroAlmaraz <aaxx1207p@gmail.com>
2020-02-24 12:18:03 -0600

    add new test dag

---
+++ alex2.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+dag = DAG("alex2",start_date=datetime(2000, 2, 22))
+
+
+def print_context(ds, **kwargs):
+    print(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag
+    )
+
+run_this
